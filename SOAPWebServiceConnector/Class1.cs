54c0793b963bcfdab673c631fb1a20d7fc683aa9
Beata Piechowicz <beata.piechowicz@student.uj.edu.pl>
2018-08-27 12:14:56 +0200

    Initial

--- SOAPWebServiceConnector/Class1.cs
+++ SOAPWebServiceConnector/Class1.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace SOAPWebServiceConnector
 {
-    public class Class1
+    public class SOAPWebServiceConnector
     {
+        public Dictionary<string, string> GetBankByBLZCode(string BLZCode)
+        {
+            BLZWebService.BLZService BankBLZWS = new BLZWebService.BLZService();
+            BLZWebService.detailsType BankDetails = new BLZWebService.detailsType();
+
+            BankDetails = BankBLZWS.getBank(BLZCode);
+
+            var BankDetailsDict = ToDictionary<string>(BankDetails);
+
+            return BankDetailsDict;
+        }
+
+
+        public static Dictionary<string, TValue> ToDictionary<TValue>(object obj)
+        {
+            var json = JsonConvert.SerializeObject(obj);
+            var dictionary = JsonConvert.DeserializeObject<Dictionary<string, TValue>>(json);
+            return dictionary;
+        }
     }
 }
